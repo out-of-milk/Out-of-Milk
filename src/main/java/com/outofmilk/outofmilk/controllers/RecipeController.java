0a962345a0f0dfc1fe24a5c2c6e436000662902a
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-04-10 13:43:23 -0500

    all controllers created

--- src/main/java/com/outofmilk/outofmilk/controllers/RecipeController.java
+++ src/main/java/com/outofmilk/outofmilk/controllers/RecipeController.java
@@ -6,7 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RecipeController {
 
+    // guest user
     @GetMapping("/")
+    public String showFindAllForm(){
+        return "findAll";
+    }
+
+    // logged-in user
+    @GetMapping("/recipe")
     public String showRecipeForm(){
         return "recipe";
     }
