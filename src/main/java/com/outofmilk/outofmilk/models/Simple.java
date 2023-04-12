adac2a547446e41edc90c82e184909680161724c
Jennie Smith <Jsmith9spiral@gmail.com>
2023-04-12 16:07:29 -0500

    gson test

---
+++ src/main/java/com/outofmilk/outofmilk/models/Simple.java
@@ -0,0 +1,15 @@
+package com.outofmilk.outofmilk.models;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Simple {
+    private String string1;
+    private String string2;
+    private int int1;
+}
