b63cfac35e393f314e814f8d17c904d30b8be473
Jennie Smith <Jsmith9spiral@gmail.com>
2023-04-17 09:40:01 -0500

    update

--- src/main/java/com/outofmilk/outofmilk/controllers/RecipeController.java
+++ src/main/java/com/outofmilk/outofmilk/controllers/RecipeController.java
@@ -24,7 +24,8 @@ public class RecipeController {
     public String showFindAllForm(Model model){
         String jsonResponse = null;
         try {
-            URL url = new URL("https://www.themealdb.com/api/json/v1/1/random.php");
+            URL url = new URL("https://www.themealdb.com/api/json/v1//random.php");
+//           put key inside
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setRequestProperty("Content-Type", "text/plain");
@@ -38,9 +39,12 @@ public class RecipeController {
             JsonObject jsonObject = gson.fromJson(jsonResponse, JsonObject.class);
             System.out.println(jsonObject);
             JsonArray mealsArray = JsonParser.parseString(jsonResponse).getAsJsonObject().getAsJsonArray("meals");
+
             System.out.println(mealsArray);
+
             for (JsonElement mealElement : mealsArray) {
                 JsonObject mealObject = mealElement.getAsJsonObject();
+
                 String idMeal = mealObject.get("idMeal").getAsString();
                 System.out.println(idMeal);
                 String strMeal = mealObject.get("strMeal").getAsString();
