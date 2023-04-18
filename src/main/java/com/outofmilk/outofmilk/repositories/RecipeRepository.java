93d48359ac76a42e531199335d52b1a2859c8c64
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-04-18 15:00:29 -0500

    adding random recipes to home page

--- src/main/java/com/outofmilk/outofmilk/repositories/RecipeRepository.java
+++ src/main/java/com/outofmilk/outofmilk/repositories/RecipeRepository.java
@@ -3,7 +3,15 @@ package com.outofmilk.outofmilk.repositories;
 import com.outofmilk.outofmilk.models.Recipe;
 import com.outofmilk.outofmilk.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Recipe findByIdMeal(long id_Meal);
+
+    @Query(value = "SELECT * FROM recipes ORDER BY RAND() LIMIT :amount", nativeQuery = true)
+    List<Recipe> selectRandomRecipes(@Param("amount") Long amount);
+
 }
