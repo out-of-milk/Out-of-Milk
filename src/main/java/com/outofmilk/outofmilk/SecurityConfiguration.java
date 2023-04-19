b65100bac79a1eca383290523d243e8817ff2d95
mcampbell379 <campbell.mitchell45@gmail.com>
2023-04-19 16:37:55 -0500

    changing method skeleton to reuse a method

--- src/main/java/com/outofmilk/outofmilk/SecurityConfiguration.java
+++ src/main/java/com/outofmilk/outofmilk/SecurityConfiguration.java
@@ -58,7 +58,7 @@ public class SecurityConfiguration {
                         "/user/{id}/dhr", // only authenticated users can remove hidden recipes
                         "/user/{id}/afr", // only authenticated users can add favorite recipes
                         "/user/{id}/ahr", // only authenticated users can add hidden recipes
-                        "/user/{id}/ari", // only authenticated users can add recipes ingredients to grocery list
+                        "/recipe/{id}/ari", // only authenticated users can add recipes ingredients to grocery list
                         "/recipe/{id}", // only authenticated users can view recipe details
                         "/user/addItemPantry", // only authenticated users can add ingredients
                         "/user/addItemGrocery" // only authenticated users can add ingredients
