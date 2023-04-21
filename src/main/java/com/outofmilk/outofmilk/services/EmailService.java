f171a580b92649cc74ec46ae6b27b75b798977a2
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-04-21 09:39:11 -0500

    fixed carousel on profile page

--- src/main/java/com/outofmilk/outofmilk/services/EmailService.java
+++ src/main/java/com/outofmilk/outofmilk/services/EmailService.java
@@ -1,11 +1,14 @@
 package com.outofmilk.outofmilk.services;
 
 import com.outofmilk.outofmilk.models.User;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service("mailService")
@@ -17,15 +20,27 @@ public class EmailService {
     @Value("${spring.mail.from}")
     private String from;
 
-    public void prepareAndSend(User user, String subject, String body) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom(from);
-        msg.setTo(user.getEmail());
-        msg.setSubject(subject);
-        msg.setText(body);
+    public void prepareAndSend(User user, String subject, String body) throws MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom(from);
+        helper.setTo(user.getEmail());
+        helper.setSubject(subject);
+
+        boolean html = true;
+        helper.setText(body, html);
+
+//        SimpleMailMessage msg = new SimpleMailMessage();
+//        msg.setFrom(from);
+//        msg.setTo(user.getEmail());
+//        msg.setSubject(subject);
+//        msg.setText(body);
 
         try{
-            this.emailSender.send(msg);
+//            this.emailSender.send(msg);
+            this.emailSender.send(message);
         }
         catch (MailException ex) {
             // simply log it and go on...
