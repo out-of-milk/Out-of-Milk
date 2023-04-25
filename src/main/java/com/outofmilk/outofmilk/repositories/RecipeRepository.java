3a71a225caab39b9eea05cb9e92d666e9133aad1
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-04-25 16:03:15 -0500

    added icon highlighting

--- src/main/java/com/outofmilk/outofmilk/repositories/RecipeRepository.java
+++ src/main/java/com/outofmilk/outofmilk/repositories/RecipeRepository.java
@@ -20,4 +20,12 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     @Query(value = "SELECT COUNT(*) FROM recipe_preferences WHERE favorite = true and recipe_id = :recipe_id", nativeQuery = true)
     long recipeLikes(@Param("recipe_id") int recipe_id);
 
+    @Query(value = "SELECT COUNT(*) FROM recipe_preferences WHERE user_id = :userId AND recipe_id = :recipe_id AND favorite = true", nativeQuery = true)
+    long findRecipeLiked(@Param("userId") long user,
+                         @Param("recipe_id") String recipe_id);
+
+    @Query(value = "SELECT COUNT(*) FROM recipe_preferences WHERE user_id = :userId AND recipe_id = :recipe_id AND hidden = true", nativeQuery = true)
+    long findRecipeHidden(@Param("userId") long user,
+                         @Param("recipe_id") String recipe_id);
+
 }
